0b44c59908da9f60644e8a97c58833b364e38b0b
Suraj Agrawal <suraj.kishor@nagarro.com>
2024-02-06 07:46:10 +0530

    Removed unused dependencies

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login')
@@ -12,10 +10,8 @@ var app = express();
 // view engine setup
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
